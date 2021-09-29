fbbb5e5d34d1dd68d814c6b17f22dde7f9347cf5
Red Cabatana <cabatanaredilyn1709@gmail.com>
2021-09-29 10:11:30 +0800

    Added subtraction feature.

--- Calculator.cpp
+++ Calculator.cpp
@@ -9,7 +9,7 @@ int main()
 //huhu
 {
 
-    int firstNumber, secondNumber, sumOfTwoNumbers;
+    int firstNumber, secondNumber, sumOfTwoNumbers, diffOfTwoNumbers;
 
     
 
@@ -29,14 +29,14 @@ int main()
     // sum of two numbers is stored in variable sumOfTwoNumbers
 
     sumOfTwoNumbers = firstNumber + secondNumber;
-
+    diffOfTwoNumbers = firstNumber - secondNumber;
 
  
 
     // Prints sum 
 
     cout << endl << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers;     
-
+    cout << endl << firstNumber << " - " <<  secondNumber << " = " << sumOfTwoNumbers;    
 
  
 
